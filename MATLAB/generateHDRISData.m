19a721baf5b373bb63dca9ee74a00bfe89c9c2f3
AlexanderFernandes96 <alex9fernandes@gmail.com>
2026-02-01 23:52:57 -0500

    Modified readability and documentation. Minor fix to make auxiliary variable (last element of v) have zero phase.

--- MATLAB/generateHDRISData.m
+++ MATLAB/generateHDRISData.m
@@ -7,9 +7,10 @@ addpath("src")
 %% Setup system model / script parameters
 systemModelParameters
 
-job_id = 0;
 % job_id = str2num(getenv("SLURM_ARRAY_TASK_ID"));
-dataDir = "datasets/HDRISData/40PdBm/";
+% dataDir = "~/scratch/datasets/HDRISData/17/";
+job_id = 0;
+dataDir = "datasets/HDRISData/17/";
 % rng(job_id)
 dataDir = dataDir + num2str(job_id) + "/";
 mkdir(dataDir);
@@ -83,25 +84,38 @@ if mod(mc_run,mc_runs/mc_run_print) == 0
     fprintElapsedTime(TSTART);
 end
 %% Create system model via channel matrices
-generateHDRISchannels
+generateHDRISchannels % This script creates uplink channels,
+                      % can generate the downlink using the uplink model.
 % Creates Channel Matrices based on scructure of the channel
 % Uplink:
 %     Hur = (N,K) UE  -> RIS
 %     Hra = (M,N) RIS -> AP
 %     Hua = (M,K) UE  -> AP
-% Downlink: channels reciprocity makes them matrix transpose equivalent
+% Downlink: reciprocal channels implies matrix transpose equivalent
 %     Hru = (K,N) RIS -> UE
 %     Har = (N,M) AP  -> RIS
 %     Hau = (K,M) AP  -> UE
+Hru = Hur.';
+Har = Hra.';
+Hau = Hua.';
+
+
+% % User priorities chosen inversely proportional to the direct-link path-loss
+% uw = zeros(K,1);
+% for k = 1:K
+%     uw(k) = norm(Hau(k,:))^2;
+% end
+% uw = uw / sum(uw);
+
 
 %% Optimize phase shifts and beamforming 
 % system model:
-% SISO:
+% Single-User SISO:
 % Y = Hua*Xu + Hra*diag(exp(1j*theta))*Hur*Xu + N (Uplink)
 % Y = Hau*Xa + Hru*diag(exp(1j*theta))*Har*Xa + N (Downlink)
-% MISO:
+% Multi-User MISO:
 % Y = W*(Hua + Hra*diag(exp(1j*theta))*Hur)*xu + N (Uplink)
-% y = (hau + hru*diag(exp(1j*theta))*Har)*W*Xa + N (Downlink)
+% y = (hau + hru*diag(exp(1j*theta))*Har)*W*Xa + N (Downlink, per user)
 
 if M == 1 && K == 1 % SISO system model
     % No beamforming matrix only RIS phase shifts to optimize
@@ -118,62 +132,23 @@ if M == 1 && K == 1 % SISO system model
     % (21) max_theta | hua + hra*diag(exp(1j*theta))*hur) |^2
     %      st. 0 <= theta <= 2*pi
 
-    theta_opt = zeros(1,N);
-    W_opt = 1;
+    theta = zeros(1,N);
+    w_opt = zeros(1,M);
     for n = 1:N
         % Knowledge of Perfect CSI
         a_kk = angle(Hua);
         b = angle(Hra(1,n));
         c = angle(Hur(n,1));
-        theta_opt(1,n) = mod(a_kk - (b+c) + pi, 2*pi) - pi;
+        theta(1,n) = mod(a_kk - (b+c) + pi, 2*pi) - pi;
     end
     theta_rand = 2*pi*rand(1,N);
-    Yopt = Hua + Hra*diag(exp(1j*theta_opt))*Hur;
+    Yopt = Hua + Hra*diag(exp(1j*theta))*Hur;
     Yrand = Hua + Hra*diag(exp(1j*theta_rand))*Hur;
 
     Ropt = Yopt'*Yopt;
     Rrand = Yrand'*Yrand;
 
-elseif K == 1 % Single-User MISO system model
-
-    % find optimal phases for the single user
-    hau = Hau(1,:);
-    hru = Hru(1,:);
-    Phi = diag(hru)*Har;
-    R = [Phi*Phi', Phi*hau'; hau*Phi', 0];
-
-    % to install cvx see: https://cvxr.com/cvx/doc/install.html
-    cvx_begin quiet
-        variable V(N+1,N+1) complex semidefinite
-        maximize(trace(R*V))
-        diag(V) == 1
-    cvx_end
-
-    % Gaussian random vector to obtain approximate best rank 1 vector
-    [U,D] = eig(V);
-    num_r = 1000; % number of SDR gaussian approximations
-    r = 1/sqrt(2)*(rand(N+1,num_r) + 1j*rand(N+1,num_r));
-    v = U*sqrt(D)*r;
-    v = exp(1j*angle(v(1:N,1:num_r) ./ v(N+1,1:num_r)));
-    v_init = v(:,1);
-    Cost_init = 0;
-    for n = 1:num_r
-        Cost = 0;
-        hau = Hau(1,:);
-        hru = Hru(1,:);
-        Cost = Cost + norm(hau + hru*diag(v(:,n))*Har);
-        if Cost > Cost_init
-            Cost_init = Cost;
-            v_init = v(:,n);
-            n_init = n;
-        end
-    end
-    theta_opt = angle(v_init);
-
-    y = hau + hru*diag(exp(1j*theta_opt(:,1)))*Har;
-    W_opt(:,1) = y' / norm(y);
-
-else % Multi-User MISO system model
+else
     % Solve for beamforming matrix and RIS phase shifts
     % Hua = MxK scalar
     % Hra = MxN row vector
@@ -213,7 +188,6 @@ else % Multi-User MISO system model
     % IEEE/CIC Int. Conf. Commun. China, ICCC 2019, pp. 735–740, Aug. 2019,
     % doi: 10.1109/ICCCHINA.2019.8855810.
 
-%     gammavar = 10^(SINRdB/10)*ones(K,1); % SINR constraint for all users
     R_best = 0;
     Ropt = zeros(K,1);
     Rrand = zeros(K,1);
@@ -262,6 +236,7 @@ else % Multi-User MISO system model
     %%% Begin iteratively solving WMMSE algorithm and RIS phase shifts
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     for iter = 1:max_AO_iterations
+
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% 2. Update U_ and L_
@@ -318,13 +293,14 @@ else % Multi-User MISO system model
             v = Rv ./ abs(Rv); % [7]
             Rv = R*v;
             Rv_norm = norm(Rv, 1);
+            v = v(N+1)' .* v; % Make auxiliary variable = 1
             v_ = v(1:N)'; % [7]
             n = n+1;
             
 %             fprintf('%i, diff: %.8f\n', n, Rv_norm - Rv_norm_prev);
             if Rv_norm - Rv_norm_prev < 1e-6
 %             fprintf('%i, diff: %.8f\n', n, Rv_norm - Rv_norm_prev);
-                theta = angle(v_);
+                theta = angle(v_); % [7]
                 break
             end
         end
@@ -357,46 +333,45 @@ else % Multi-User MISO system model
                 end
             end
             Rate = Rate + log2(1 + norm(hk*W(:,k))^2 / intf_opt);
-            if Rate > R_best
-                R_best = Rate;
-                % Save optimal beamforming matrix and theta
-                W_opt = sqrt(P)*W/norm(W,'fro');
-                theta_opt = theta;
-            end
         end
-%         fprintf('iter %i  \tBest: %.4f \tRate: %.4f\n', iter, R_best, Rate);
+        if Rate > R_best
+            R_best = Rate;
+            % Save optimal beamforming matrix and theta
+            W_opt = sqrt(P)*W/norm(W,'fro');
+            theta_opt = theta;
+        end
+        fprintf('iter %i  \tBest: %.4f \tRate: %.4f\n', iter, R_best, Rate);
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% End Alternating Optimization
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     end % End AO
-end
 
-% Compare optimal phases vs random phases given optimal beamforming
-% with true channels
-for k = 1:K 
-    hau = Hau(k,:);
-    hru = Hru(k,:);
-    theta_rand = 2*pi*rand(1,N) - pi;
-    h_opt = hau + hru*diag(exp(1j*theta_opt))*Har;
-    h_rand = hau + hru*diag(exp(1j*theta_rand))*Har;
-    intf_opt = 10^(NdBm/10);
-    intf_rand = 10^(NdBm/10);
-    for l = 1:K
-        if l ~= k
-            intf_opt = intf_opt + norm(h_opt*W_opt(:,l))^2;
-            intf_rand = intf_rand + norm(h_rand*W_opt(:,l))^2;
+    % Compare optimal phases vs random phases given optimal beamforming
+    % with true channels
+    for k = 1:K 
+        hau = Hau(k,:);
+        hru = Hru(k,:);
+        theta_rand = 2*pi*rand(1,N) - pi;
+        h_opt = hau + hru*diag(exp(1j*theta_opt))*Har;
+        h_rand = hau + hru*diag(exp(1j*theta_rand))*Har;
+        intf_opt = 10^(NdBm/10);
+        intf_rand = 10^(NdBm/10);
+        for l = 1:K
+            if l ~= k
+                intf_opt = intf_opt + norm(h_opt*W_opt(:,l))^2;
+                intf_rand = intf_rand + norm(h_rand*W_opt(:,l))^2;
+            end
         end
+        Ropt(k) = log2(1 + norm(h_opt*W_opt(:,k))^2 / intf_opt);
+        Rrand(k) = log2(1 + norm(h_rand*W_opt(:,k))^2 / intf_rand);
     end
-    Ropt(k) = log2(1 + norm(h_opt*W_opt(:,k))^2 / intf_opt);
-    Rrand(k) = log2(1 + norm(h_rand*W_opt(:,k))^2 / intf_rand);
+    
+    fprintf('R_opt: %.4f \tR_rand: %.4f\n', sum(Ropt), sum(Rrand));
+
+    theta_opt = theta_opt.'; % stack all phases into one row vector
+    w_opt = W_opt(:).';
 end
-
-fprintf('R_opt: %.4f \tR_rand: %.4f\n', sum(Ropt), sum(Rrand));
-
-theta_opt = theta_opt.'; % stack all phases into one row vector
-w_opt = W_opt(:).';
-
 
 % Example:     hur <=> Hur(:) for Hur = N by K matrix
 % vectorize:   hur = reshape(Hur, [N*K,1]), stack columns into one column
@@ -415,7 +390,6 @@ fprintf("\n------------------------------------------------------------\n")
 fprintf("Mean Sum Rate over montecarlo runs:\n");
 fprintf("Optimized RIS: %.4f +/- %.4f\n", mean(Ropt2_mc, 1), var(Ropt2_mc, 1));
 fprintf("Random RIS: %.4f +/- %.4f\n", mean(Rrand2_mc, 1), var(Rrand2_mc, 1));
-fprintf("Average Channel Error:\n")
 
 %% Save data
 save(fileSaveName + ".mat", vars2save{:})
